9135121a975e3288affe4b2f1b13ee0aa1af9785
Greg McCullough <gmccullo@redhat.com>
2024-10-07 08:18:21 -0400

    Accept column name as argument Default to __ALL__ columns.

--- trello_print.rb
+++ trello_print.rb
@@ -2,23 +2,26 @@
 
 require 'trello'
 
+# SidePiece board ID
+BOARD_ID = ENV['TRELLO_BOARD_ID']
+ALL_LISTS = '__ALL__'
+
 # Set up Trello API credentials
 Trello.configure do |config|
   config.developer_public_key = ENV['TRELLO_DEVELOPER_PUBLIC_KEY']
   config.member_token = ENV['TRELLO_MEMBER_TOKEN']
 end
 
-# SidePiece board ID
-board_id = ENV['TRELLO_BOARD_ID']
+column_name = ARGV.empty? ? ALL_LISTS : ARGV.join(' ')
 
 # Get the board
-board = Trello::Board.find(board_id)
+board = Trello::Board.find(BOARD_ID)
 
 # Print titles of cards for each column (list) on the board
 skipped_boards = []
 
 board.lists.each do |list|
-  if list.name.include?('Set ')
+  if list.name.include?(column_name) || column_name == ALL_LISTS
     puts "** #{list.name}"
     list.cards.each do |card|
       puts card.name
